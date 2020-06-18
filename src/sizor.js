b9dcf3d0c93f46b16b5fe319fb85f6b7223980ce
Hannah Scott <Hannah.Scott@flgroup.co.uk>
2020-06-18 12:47:02 +0100

    Moved media into folder and created src folder with js in

---
+++ src/sizor.js
@@ -0,0 +1,42 @@
+var currentPage = 0;
+var pages = [
+    "FRONT", 
+    "INNERFRONT",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "BACK"
+];
+
+function getPagePath(page) {
+    return "media/panels/" + page + ".png";
+}
+
+function setup() {
+    console.log("Current page: ", currentPage)
+    setPage(currentPage);
+}
+
+function nextPage(id) {
+    if (id < pages.length - 1) {
+        console.log(id);
+        setPage(id + 1);
+    }
+}
+
+function previousPage(id) {
+    if (id > 0){
+        console.log(id);
+        setPage(id - 1);
+    }
+}
+
+function setPage(id) {
+    currentPage = id;
+    document.getElementById("currentPage").setAttribute("src", getPagePath(pages[currentPage]));
+    document.getElementById("pageNum").innerHTML = "Page number: " + (currentPage + 1) + " / " + pages.length;
+}
+
+setup();
